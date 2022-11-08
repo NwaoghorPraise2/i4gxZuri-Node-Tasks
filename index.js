8b3a295d4b62193543c902ebaddd9874deb1247f
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2022-11-08 21:50:43 +0200

    Submit

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const app = express();
 app.use(json());
 
 app.use("/", routes);
-
-
+app.use("/api", routes);
 
 //get All Flights
 let getAllFlights = async (req, res) => {
@@ -26,6 +25,23 @@ let getAllFlights = async (req, res) => {
         res.status(500).json({ message: err.message});
     }
 };
+
+// get single flight
+let getSingleFlight =  async (req, res) => {
+    try {
+        let id = await req.params.id;
+        const getFlight = flightList.find((flight) => flight.id === id);
+        res.status(200).json({
+            message: 'Flight Found',
+            getFlight
+        });
+    } catch (err) {
+        res.json({
+            message: err.message
+        });
+    }
+
+}
 
 // add or book flight
 let bookFlight = async (req, res) => {
@@ -49,8 +65,51 @@ let bookFlight = async (req, res) => {
     }
 };
 
-routes.get('/flights', getAllFlights)
-    .post('/flights', bookFlight);
+//update flight
+let updateFlight = async (req, res) => {
+    try{
+    let id = req.params.id;
+    const getFlight = flightList.find((flight) => flight.id === id);
+    const { tittle, price } = await req.body;
+    getFlight.tittle = tittle;
+    getFlight.price =price;
+    getFlight.date = new Date().toLocaleDateString();
+    getFlight.time = new Date().toLocaleTimeString();
+    res.status(201).json(
+        {
+        message:'Flight Updated',
+        getFlight
+        }
+    );
+    } catch (err) {
+    res.status(500).json({
+        message : err.message
+    });    
+    }
+};
+
+//delete flight 
+let deleteFlight = async (req, res) => {
+    try{
+        let id = await req.params.id;
+        const getFlight = flightList.find((flight) => flight.id === id);
+        flightList.splice(flightList.indexOf(getFlight), 1);
+        res.status(200).json({
+            message: 'Flight Deleted',
+            getFlight
+        });
+    }catch(err){
+    res.status(500).json({
+        message: err.message
+    });
+    }
+};
+routes.get('/api', getAllFlights)
+    .get('/api/:id', getSingleFlight)
+    .delete('/api/:id', deleteFlight)
+    .put('/api/:id', updateFlight)
+    .post('/api', bookFlight);
+    
 
 const port = process.env.PORT || 3100;
 
