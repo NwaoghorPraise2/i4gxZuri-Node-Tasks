d44bfa4073f0b9b3f174eca76cf416f38cdaa75b
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2022-11-10 22:50:17 +0200

    Project-V1-Ready

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,30 +1,132 @@
 const User = require('../models/userModel');
+
     //get all users
     let getAllUsers = async (req, res) => {
         try {
-            let user = await User.find();
+            let users = await User.find();
+            if (users.length === 0) {
+                return res.status(404).json({
+                    success:false,
+                    message: 'No user not found'
+                });
+            };
             res.status(200).json({
                 success: true,
                 message: 'Users Found',
-                user
+                users
             });
-        } catch (error) {
+        } catch (err) {
             res.status(500).json({
                 success: false,
                 message: 'Internal Server Error',
-                error: error.message
+                error: err.message
             });
         }
     }
     
     //get single users
+    let getSingleUSer = async (req, res) => {
+        try {
+            let id = {_id: req.params.id};
+            let user = await User.findOne(id);
+                if (!user) return res.status(404).json({
+                    success: false,
+                    message: 'user not found'
+                });
+                res.status(200).json({
+                    success: true,
+                    message: 'User Found',
+                    user
+                });
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                message: 'Internal Error',
+                error: err.message
+            });
+        }
+    };
+    
+    //create user
+    let createUser = async (req, res) => {
+        try {
+            let user = await req.body;
+            // console.log(user);
+            let createdUser = await User.create(user);
+            if (!createdUser) return res.status(404).json({
+                message: false,
+                message: 'User Creation failed'
+            })
+            return res.status(201).json({
+                success: true,
+                message: 'User Creation Successful',
+                user: createdUser
+            })
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                message: 'internal matters',
+                error: err.message
+            });
+        }
+    };
     
     //update users
+    let updateUser = async (req, res) => {
+        try {
+            let id = {_id: req.params.id}
+            let user = await req.body;
+            let updateUser = await User.findByIdAndUpdate(id, user, {new: true});
+        
+        if (!updateUser) return res.status(500).json({
+            success: false,
+            message: 'User not updated'
+        });
+        
+        return res.status(200).json({
+            success: true,
+            message: 'User Updated',
+            updateUser
+        });
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                message: 'Internal Error',
+                error: err.body
+            });            
+        }
+    };
     
     //Delete Users
-    
+    let deleteUser = async (req, res) => {
+        try {
+            let id = {_id: req.params.id};
+            let deleteUser = await User.findOneAndDelete(id);
+            
+            if (!deleteUser) return res.status(400).json({
+                success: false,
+                message: 'User not deleted'
+            });
+            
+            return res.status(200).json({
+                success: true,
+                message: "User Successfully Deleted",
+                deletedUser: deleteUser
+            });
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                message: 'Internal Errors',
+                error: err.message
+            })
+        }
+    }
     
     //Export Modules
     module.exports = {
-        getAllUsers
+        getAllUsers,
+        getSingleUSer,
+        createUser,
+        updateUser,
+        deleteUser
     };
--- src/router/userRoutes.js
+++ src/router/userRoutes.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const controllers = require('../controllers/userController');
 
 
-router.get('/', controllers.getAllUsers)
-
+router
+    .get('/', controllers.getAllUsers)
+    .get('/:id', controllers.getSingleUSer)
+    .post('/', controllers.createUser)
+    .put('/:id', controllers.updateUser)
+    .delete('/:id', controllers.deleteUser);
+    
 module.exports = router;
