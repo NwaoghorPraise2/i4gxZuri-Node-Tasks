b0bac7790c7e4f8da810c64f29220ff826d1e2a0
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2022-11-15 13:55:29 +0200

    Submit

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const {config} = require('dotenv');
 config();
 let connect = async (uri) => {
     try {
-        mongoose.connect(uri || process.env.MONGO_DB);
+        mongoose.connect( uri || process.env.MONGO_DB );
         console.log('Connected to MongoDB');
     } catch (err) {
         console.log(err.message);
