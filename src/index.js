5772b0afaaaa638ac4c39983627e8021fd4f57a8
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2022-11-15 13:58:30 +0200

    Submit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const {json, urlencoded} = require('express');
-const env = require('dotenv').config();
+require('dotenv').config();
 const connect = require('./config/database');
 const userRoute = require('./router/todosRoutes');
 
+const port = process.env.PORT || 2000; 
+
+//Calling the Express Function
 const app = express();
 
+//Setting Middlewares
 app.use(json());
 app.use('/api', userRoute);
 
-
+//Connecting Mongo DB
 connect(process.env.ONLINE_DB);
 
-
-const port = process.env.PORT || 2000; 
-
-
+//server
 app.listen(port, () => {
     console.log(`server started on ${port}`);
 });
